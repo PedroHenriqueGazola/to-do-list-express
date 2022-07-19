81ba610ea598e06d8e764acd598d67f15a6d1cb3
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-19 20:46:50 -0300

    Criando o servidor

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => { // req = requisiçao / res = resposta
+    res.send('<h1>Minha lista de tarefas</h1>')
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
