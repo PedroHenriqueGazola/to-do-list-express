b7fff8c956d2d2fe0f7637fd7e300358d43749ad
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-21 20:54:13 -0300

    Metodo POST

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -5,5 +5,9 @@ router.get('/', (req, res) => {
     console.log('olá')
     res.send()
 }); 
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.status(200).send()
+});
 
 module.exports = router; 
