8b8171f93c06b53fc454f6dfed335886201150b0
Pedro Gazola <ph-gazola@hotmail.com>
2022-07-21 21:06:52 -0300

    Put e Delete

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -8,11 +8,19 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     console.log(req.body)
     res.status(200).send()
-});
+}); // Post serve para criar um registro
 
 router.get("/:id", (req, res) => {
     console.log(req.params.id)
     res.send(`ID: ${req.params.id}`)
-})
+}) // get serve para buscar informações
 
+router.put("/:id", (req, res) => {
+    console.log(req.params.id)
+    res.send(`ID: ${req.params.id}`)
+}) // put serve para atualizar informações
+router.delete("/:id", (req, res) => {
+    console.log(req.params.id)
+    res.send(`ID: ${req.params.id}`)
+}) // delete serve para deletar informações
 module.exports = router; 
